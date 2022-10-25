daf96455a7269effd83da0bff2e3badec7ba280b
mannai marwa <marwa.mannai@esprit.tn>
2022-10-25 23:11:17 +0100

    crud

--- citoyen.cpp
+++ citoyen.cpp
@@ -1,69 +1,84 @@
 #include "citoyen.h"
 #include<QSqlQuery>
+#include<QSqlQueryModel>
 #include<QtDebug>
 #include<QObject>
 #include<QString>
-
 #include<QStringList>
+#include<QDate>
+
+
 Citoyen::Citoyen()
 {
-     id=0;
+     cin=0;
      nom="";
      prenom="";
      adresse="";
-
      num=0;
+     date=QDate(2000,01,01);
+     nationalite="";
 }
-Citoyen::Citoyen(int id,QString nom,QString prenom,QString adresse,int num)
+
+Citoyen::Citoyen(int cin,QString nom,QString prenom,QString adresse,int num,QDate date,QString nationalite)
 {
-this->id=id;
+this->cin=cin;
 this-> nom= nom;
 this-> prenom= prenom;
 this-> adresse= adresse;
 this-> num= num;
+this->date=date;
+this-> nationalite= nationalite;
+
 }
 
-int Citoyen::getid(){return id;}
+int Citoyen::getcin(){return cin;}
 QString Citoyen::getnom(){return  nom;}
 QString Citoyen::getprenom(){return  prenom;}
 QString Citoyen::getadresse(){return adresse;}
 int Citoyen::getnum(){return num;}
+QDate Citoyen::getdate(){ return date;}
+QString Citoyen::getnationalite(){return  nationalite;}
 
 
 
-void Citoyen::setid(int id){this->id=id;}
+void Citoyen::setcin(int cin){this->cin=cin;}
 void Citoyen::setnom(QString  nom ){this-> nom= nom;}
 void Citoyen::setprenom(QString prenom){this->prenom=prenom;}
 void Citoyen::setadresse(QString adresse){this->adresse=adresse;}
 void Citoyen::setnum(int  num){this-> num= num;}
+void Citoyen::setdate(QDate date){ this->date=date;}
+void Citoyen::setnationalite(QString  nationalite ){this-> nationalite= nationalite;}
+
 
 
 bool Citoyen::ajouterCitoyen()
 {
 
      QSqlQuery query;
-     QString id_string=QString::number(id);
+     QString cin_string=QString::number(cin);
       QString  num_string=QString::number( num);
-           query.prepare("INSERT INTO Citoyen (id, nom, prenom,adresse, num) "
-                         "VALUES (:id, :nom, :prenom, :adresse,:num)");
-           query.bindValue(":id", id_string);
+           query.prepare("INSERT INTO Citoyen (cin, nom, prenom,adresse, num, date_naissance,nationalite) "
+                         "VALUES (:cin, :nom, :prenom, :adresse,:num,:date_naissance,:nationalite)");
+           query.bindValue(":cin", cin_string);
            query.bindValue(":nom",  nom);
            query.bindValue(":prenom", prenom);
            query.bindValue(":adresse", adresse);
            query.bindValue(":num",  num_string);
+           query.bindValue(":date_naissance",date);
+           query.bindValue(":nationalite", nationalite);
 
          return  query.exec();
 
  }
 
 
- bool Citoyen::supprimerCitoyen(int id)
+ bool Citoyen::supprimerCitoyen(int cin)
  {
      QSqlQuery query;
-     QString id_string=QString::number(id);
+     QString cin_string=QString::number(cin);
      QString  num_string=QString::number( num);
-           query.prepare("DELETE from Citoyen where id=:id");
-           query.bindValue(0, id);
+           query.prepare("DELETE from Citoyen where cin=:cin");
+           query.bindValue(0, cin);
 
          return  query.exec();
 
@@ -74,21 +89,25 @@ QSqlQueryModel*Citoyen::afficherCitoyen()
 {
     QSqlQueryModel*model=new QSqlQueryModel();
  model->setQuery("SELECT * FROM Citoyen");
-    model->setHeaderData(0, Qt::Horizontal, QObject::tr("id Citoyen"));
+    model->setHeaderData(0, Qt::Horizontal, QObject::tr("cin Citoyen"));
     model->setHeaderData(1, Qt::Horizontal, QObject::tr(" nom Citoyen"));
       model->setHeaderData(2, Qt::Horizontal, QObject::tr(" prenom Citoyen"));
       model->setHeaderData(3, Qt::Horizontal, QObject::tr("adresse Citoyen "));
         model->setHeaderData(4, Qt::Horizontal, QObject::tr("numero tel Citoyen"));;
+         model->setHeaderData(5, Qt::Horizontal, QObject::tr("date de naissance Citoyen"));;
+          model->setHeaderData(6, Qt::Horizontal, QObject::tr("nationalite Citoyen"));;
            return model;
 }
-bool Citoyen::modifierCitoyen(int id, QString nom, QString prenom, QString adresse, int num)
+bool Citoyen::modifierCitoyen(int cin, QString nom, QString prenom, QString adresse, int num , QDate date , QString nationalite)
 {
     QSqlQuery query;
-    query.prepare("UPDATE Citoyen set id=:id,nom=:nom,prenom=:prenom,adresse=:adresse,num=:num WHERE id=:id");
-    query.bindValue(":id",id);
+    query.prepare("UPDATE Citoyen set cin=:cin,nom=:nom,prenom=:prenom,adresse=:adresse,num=:num,date:=date,nationalite:=nationalite WHERE cin=:cin");
+    query.bindValue(":cin",cin);
     query.bindValue(":nom",nom);
     query.bindValue(":prenom",prenom);
     query.bindValue(":adresse",adresse);
     query.bindValue(":num",num);
+    query.bindValue(":date",date);
+    query.bindValue(":nationalite",nationalite);
 
     return (query.exec());}
--- citoyen.h
+++ citoyen.h
@@ -10,36 +10,45 @@ class Citoyen
 {
 public:
     Citoyen();
-    Citoyen(int,QString,QString,QString,int);
+    Citoyen(int,QString,QString,QString,int,QDate,QString);
 
-    //getter
-    int getid();
-    QString getnom();
-     QString getprenom();
-    QString getadresse();
-     int getnum();
+//getter
+int getcin();
+QString getnom();
+QString getprenom();
+QString getadresse();
+int getnum();
+QDate getdate();
+QString getnationalite();
 
-      //setter
-    void setid(int);
-    void setnom(QString);
-    void setprenom(QString);
-    void setadresse(QString);
-     void setnum(int);
 
-    //methode
-     bool ajouterCitoyen();
-     QSqlQueryModel* afficherCitoyen();
-    bool supprimerCitoyen(int);
-    bool modifierCitoyen(int,QString,QString,QString,int );
+
+//setter
+void setcin(int);
+void setnom(QString);
+void setprenom(QString);
+void setadresse(QString);
+void setnum(int);
+void setdate(QDate);
+void setnationalite(QString);
+
+//methode
+bool ajouterCitoyen();
+QSqlQueryModel* afficherCitoyen();
+bool supprimerCitoyen(int);
+bool modifierCitoyen(int,QString,QString,QString,int,QDate,QString);
+
 
 
 
 private:
-    int id;
-   QString nom;
- QString prenom;
- QString adresse ;
- int num ;
+int cin;
+QString nom;
+QString prenom;
+QString adresse ;
+int num ;
+QDate date;
+QString nationalite;
 
 
 };
